17779006869ba99ff025ec487809ac5cd09a4233
ballparts <poppe076@gmail.com>
2015-02-02 22:14:11 -0700

    built file-building logic

--- playslow1.py
+++ playslow1.py
@@ -4,8 +4,11 @@ import math, sys
 s = Server().boot()
 s.start()
 
-def slow(playback_speed):
+#def slow(playback_speed):
+if 3>2:
     dur = 2
+    
+    playback_speed = .5
     if playback_speed == 0:
         playback_speed = pow(10,-100)
     i = Input()
@@ -24,11 +27,14 @@ def slow(playback_speed):
 
     tab = NewTable(dur, chnls=2)
     transpo_to_normal = math.log(1.0 / playback_speed, 2) * 12
-    j = Harmonizer(i, transpo=transpo_to_normal)
-    k = Gate(j, thresh=-70, falltime=0.02, lookahead=20.0).mix(2)
+    j = Harmonizer(i, transpo=transpo_to_normal).mix(2)
+    k = Gate(j, thresh=-70, falltime=0.02, lookahead=20.0)
     rec = TableRec(k, tab)
     a = TableRead(table=tab, freq=playback_speed/dur).stop()
     env = Follower(i)
     th = Thresh(env, .2)
     tf = TrigFunc(th, start)
     tr = TrigFunc(rec['trig'], stop)
+
+#slow(.5)
+s.gui(locals())
